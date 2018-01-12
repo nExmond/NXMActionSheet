09cba74dc159b2e7d764c5f52897866dd6024790
nExmond <rnpfr254@naver.com>
2018-01-13 01:37:02 +0900

    ...

--- Example/NXMActionSheet/CustomActionSheet.swift
+++ Example/NXMActionSheet/CustomActionSheet.swift
@@ -2,8 +2,8 @@
 //  CustomActionSheet.swift
 //  NXMActionSheet_Example
 //
-//  Created by Twinny on 2018. 1. 12..
-//  Copyright © 2018년 CocoaPods. All rights reserved.
+//  Created by nExmond on 2018. 1. 12..
+//  Copyright © 2018년 nExmond. All rights reserved.
 //
 
 import UIKit
--- Example/NXMActionSheet/CustomDatePickerView.swift
+++ Example/NXMActionSheet/CustomDatePickerView.swift
@@ -2,8 +2,8 @@
 //  CustomDatePickerView.swift
 //  NXMActionSheet_Example
 //
-//  Created by Twinny on 2018. 1. 12..
-//  Copyright © 2018년 CocoaPods. All rights reserved.
+//  Created by nExmond on 2018. 1. 12..
+//  Copyright © 2018년 nExmond. All rights reserved.
 //
 
 import UIKit
